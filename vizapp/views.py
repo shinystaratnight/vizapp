98349190a1218a7b1a1da375b0d5018085429d36
Rising Star <star-atnight@outlook.com>
2022-01-27 09:17:30 +0800

    add templates

--- vizapp/views.py
+++ vizapp/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from core.utils import get_nombres_series
 
 def home(request):
-    nombres_series = get_nombres_series()
-    return HttpResponse('Hello')
+    return render(request, 'home.html', {})
