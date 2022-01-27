387350110f6b6db1b4b2847a434f461184ee2911
Rising Star <star-atnight@outlook.com>
2022-01-27 08:18:42 +0800

    Add vizapp app

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import vizapp
 
 urlpatterns = [
+    path('', include('vizapp.urls')),
     path('admin/', admin.site.urls),
 ]
